6e5ade078c6283e50a6c28aeb4f1c6399e6f6d7d
Yasir Khan <yasir_electronics@yahoo.com>
2019-01-02 10:38:30 -0800

    Increase threshold for moving formward
    Previous threshold was low enough for the rover to
    collide and get stuck with obstacles.
    
    Signed-off-by: Yasir Khan <yasir_electronics@yahoo.com>

--- code/drive_rover.py
+++ code/drive_rover.py
@@ -59,9 +59,9 @@ class RoverState():
         # of navigable terrain pixels.  This is a very crude form of knowing
         # when you can keep going and when you should stop.  Feel free to
         # get creative in adding new fields or modifying these!
-        self.stop_forward = 50 # Threshold to initiate stopping
-        self.go_forward = 500 # Threshold to go forward again
-        self.max_vel = 2 # Maximum velocity (meters/second)
+        self.stop_forward = 50 + 50# Threshold to initiate stopping
+        self.go_forward = 500 + 50 # Threshold to go forward again
+        self.max_vel = 1 # Maximum velocity (meters/second)
         # Image output from perception step
         # Update this image to display your intermediate analysis steps
         # on screen in autonomous mode
